d117a3c9775a9c8234f85031c5cb578994ab0f05
SabrinaHikariYamauti <hikarijpfriends@hotmail.com>
2021-10-25 21:57:10 -0300

    added Weather Screen

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+//Import Libraries
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Weather from './WeatherDetail/Weather';
+//Import Styles
+import ModeStyles from './Styles/ModeStyles';
+
+import Telas from './Telas/Telas';
+
+
+const DashboardScreen = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View styles={styles.container}>
+      <Text>(Screen2)</Text>
+      <Text>Dashboard</Text>
+      <Button
+        title='Back'
+        onPress={() => navigation.goBack()}
+      ></Button>
+      <Button
+        title='Go to Home'
+        onPress={() => navigation.popToTop()}
+      ></Button>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    //<Telas />
+    <Weather />
+  );
+}
+
+export default App;
